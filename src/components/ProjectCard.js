97edf363da279d4d0d43dc89d508f02b8bbfb962
Ganesh Shinde <sindheganesh2004@gmail.com>
2023-06-30 18:04:59 +0530

    Feature: provided the links to each projects

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,15 +1,21 @@
 import { Col } from "react-bootstrap";
 
-export const ProjectCard = ({ title, description, imgUrl }) => {
+export const ProjectCard = ({ title, description, imgUrl, link }) => {
   return (
     <Col size={12} sm={6} md={4}>
       <div className="proj-imgbx">
         <img src={imgUrl} />
         <div className="proj-txtx">
           <h4>{title}</h4>
-          <span>{description}</span>
+          <span>
+            {description}{" "}
+            <a href={link} style={{ color: "black" }}>
+              {" "}
+              Link
+            </a>
+          </span>
         </div>
       </div>
     </Col>
-  )
-}
+  );
+};
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -22,6 +22,8 @@ import projImg19 from "../assets/img/musix.avif";
 import projImg20 from "../assets/img/dice.webp";
 import projImg21 from "../assets/img/guga.jpeg";
 import projImg22 from "../assets/img/sb.webp";
+import projImg23 from "../assets/img/firecrud.jpg";
+import projImg24 from "../assets/img/port3d.png";
 import colorSharp2 from "../assets/img/color-sharp2.png";
 import "animate.css";
 import TrackVisibility from "react-on-screen";
@@ -33,36 +35,56 @@ export const Projects = () => {
       description:
         "Sociopedia is a social media web app built using the MERN stack",
       imgUrl: projImg1,
+      link: "https://github.com/Ganeshshinde-2003/Sociopedia_MERN",
     },
     {
       title: "Twitter Clone",
       description:
         "A Twitter clone that allows users to post tweets after logging in",
       imgUrl: projImg2,
+      link: "https://github.com/Ganeshshinde-2003/Post-Your-Tweets",
     },
     {
       title: "Amazon Mini",
       description:
         "This project is a mini clone of Amazon built using React JS and CSS",
       imgUrl: projImg3,
+      link: "https://github.com/Ganeshshinde-2003/amazon-starterpack",
+    },
+    {
+      title: "Portfolio",
+      description:
+        "This is my personal portfolio website, where I showcase my work & skills",
+      imgUrl: projImg24,
+      link: "https://github.com/Ganeshshinde-2003/My_Portfolio_app",
     },
     {
       title: "PizzaHut",
       description:
         "Pizzeria is a pizza ordering web app built using React and React Router",
       imgUrl: projImg4,
+      link: "https://github.com/Ganeshshinde-2003/pizzahut-react",
     },
     {
       title: "ReactCurd",
       description:
         "React-based web app, users can interaction with user data through crud operations",
       imgUrl: projImg5,
+      link: "https://github.com/Ganeshshinde-2003/React-Curd-Operations",
     },
     {
       title: "Portfolio",
       description:
         "This is my personal portfolio website, where I showcase my work & skills",
       imgUrl: projImg6,
+      link: "https://github.com/Ganeshshinde-2003/protofolio",
+    },
+    {
+      title: "FirebaseCrud",
+      description:
+        "CRUD operations using Firebase Firestore for managing hotel documents",
+      imgUrl: projImg23,
+      link: "https://github.com/Ganeshshinde-2003/React-Firebase-Crud-Operation",
     },
   ];
 
@@ -72,34 +94,40 @@ export const Projects = () => {
       description:
         "Replicating the seamless messaging app WhatsApp using Flutter and Firebase",
       imgUrl: projImg7,
+      link: "https://github.com/Ganeshshinde-2003/Flutter_WhatsApp_Clone",
     },
     {
       title: "Gossip",
       description: "GOSSIP allows users to create and join groups for chatting",
       imgUrl: projImg8,
+      link: "https://github.com/Ganeshshinde-2003/Gossip-Chat-App",
     },
     {
       title: "Chat-Assist",
       description:
         "Voice Assist is a voice-activated virtual assistant that generate images and answer questions",
       imgUrl: projImg9,
+      link: "https://github.com/Ganeshshinde-2003/Chat-Assist",
     },
     {
       title: "Covid Tracker",
       description:
         "A Flutter application that allows users to track COVID-19 statistics worldwide",
       imgUrl: projImg10,
+      link: "https://github.com/Ganeshshinde-2003/Covid-Tracker-Flutter-App",
     },
     {
       title: "ToDo App",
       description: "A simple ToDo list app for managing tasks",
-      imgUrl: projImg11,
+      imgUrl: projImg12,
+      link: "https://github.com/Ganeshshinde-2003/ToDo-Flutter-App",
     },
     {
       title: "NoteTaker",
       description:
         "A simple note-taking app. It allows users to create, edit, and delete notes",
-      imgUrl: projImg12,
+      imgUrl: projImg11,
+      link: "https://github.com/Ganeshshinde-2003/Flutter-Notes-Taking-App-Using-Hive-DataBase",
     },
   ];
 
@@ -108,59 +136,69 @@ export const Projects = () => {
       title: "Fight Game",
       description: "A game project that allows two players to fight",
       imgUrl: projImg13,
+      link: "https://github.com/Ganeshshinde-2003/Fight_TO_The_Death_Javascript",
     },
     {
       title: "Weather App",
       description:
         "The application displays the current temperature, humidity, wind speed",
       imgUrl: projImg14,
+      link: "https://github.com/Ganeshshinde-2003/weather-app-javascript",
     },
     {
       title: "Url Shortener",
       description:
-        "this allows users to shorten long URLs into shorter, more manageable links",
+        "This allows users to shorten long URLs into shorter, more manageable links",
       imgUrl: projImg15,
+      link: "https://github.com/Ganeshshinde-2003/CodeClause_URL_Shortener",
     },
     {
       title: "Note Taker",
       description:
         "This is a simple note-taking application built using JavaScript, HTML, and CSS",
       imgUrl: projImg16,
+      link: "https://github.com/Ganeshshinde-2003/CodeClause_Note_Taking_Application",
     },
     {
       title: "Personal Blog",
       description:
         "This is my personal blog website, where I write about technology",
       imgUrl: projImg17,
+      link: "https://github.com/Ganeshshinde-2003/CodeClause_Personal_Blog",
     },
     {
       title: "Password Gen",
       description:
         "Generates a random password based on user-specified criteria",
       imgUrl: projImg18,
+      link: "https://github.com/Ganeshshinde-2003/passward-generator",
     },
     {
       title: "Music App",
       description:
         "This is a web app music app that allows users to listen to and discover new music",
       imgUrl: projImg19,
+      link: "https://github.com/Ganeshshinde-2003/MYMUSIC",
     },
     {
       title: "Dice Game",
       description:
         "Two-player game, Players take turns rolling dice to try to score of 100",
       imgUrl: projImg20,
+      link: "https://github.com/Ganeshshinde-2003/dice-game",
     },
     {
       title: "Guess Game",
       description: "This project is a simple guessing game between 1 and 20",
       imgUrl: projImg21,
+      link: "https://github.com/Ganeshshinde-2003/guess-game-number",
     },
     {
       title: "Starbucks",
       description:
         "This is a Starbucks clone project created using only HTML and CSS",
       imgUrl: projImg22,
+      link: "https://github.com/Ganeshshinde-2003/starbucks-clone",
     },
   ];
 
